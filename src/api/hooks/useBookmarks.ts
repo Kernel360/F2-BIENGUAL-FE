fc16731ad483b65c4a20e34814e8474322e614eb
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-10-31 15:19:49 +0900

    feat : optimistic update수정.onMutate, onError, onSettled 속성으로 구현

--- src/api/hooks/useBookmarks.ts
+++ src/api/hooks/useBookmarks.ts
@@ -6,6 +6,7 @@ import {
   BookmarkListResponse,
   BookmarkByContentIdResponse,
   Bookmark,
+  BookmarkByContentId,
 } from '../../types/Bookmark';
 import {
   fetchBookmarksByContentId,
@@ -31,7 +32,7 @@ export const useFetchAllBookmarks = () => {
   });
 };
 
-// 북마크 생성 훅
+// 북마크 생성 훅 : optimistic update적용
 export const useCreateBookmark = (contentId: number) => {
   const queryClient = useQueryClient();
 
@@ -41,17 +42,50 @@ export const useCreateBookmark = (contentId: number) => {
     { sentenceIndex: number; wordIndex?: number; description?: string }
   >({
     mutationFn: (newBookmark) => createBookmark(contentId, newBookmark),
-    // TODO(@godhyzzang) : onError처리
-    onSettled: async () => {
-      const result = await queryClient.invalidateQueries({
+    // when mutation is called:
+    onMutate: async (newBookmark) => {
+      await queryClient.cancelQueries({ queryKey: ['bookmarks', contentId] });
+      // Snapshot the previous value
+      const previousBookmarks =
+        queryClient.getQueryData<BookmarkByContentIdResponse>([
+          'bookmarks',
+          contentId,
+        ]);
+      // 캐시에 optimistically update
+      queryClient.setQueryData(['bookmarks', contentId], {
+        ...previousBookmarks,
+        data: {
+          bookmarkList: [
+            ...(previousBookmarks?.data.bookmarkList || []),
+            newBookmark,
+          ],
+        },
+      });
+      return { previousBookmarks };
+    },
+
+    // 에러가 발생할 경우 onMute에서 보관한 캐시데이터가 있다면 복원
+    onError: (err, newBookmark, context) => {
+      const ctx = context as { previousBookmarks?: BookmarkByContentId[] };
+
+      if (ctx.previousBookmarks) {
+        queryClient.setQueryData(
+          ['bookmarks', contentId],
+          ctx.previousBookmarks,
+        );
+      }
+    },
+
+    // 성공 여부와 관계없이 항상 refetch
+    onSettled: () => {
+      queryClient.invalidateQueries({
         queryKey: ['bookmarks', contentId],
       });
-      return result;
     },
   });
 };
 
-// 북마크 수정 훅
+// 북마크 수정 훅 : optimistic update적용
 export const useUpdateBookmark = (contentId: number) => {
   const queryClient = useQueryClient();
 
@@ -62,19 +96,90 @@ export const useUpdateBookmark = (contentId: number) => {
   >({
     mutationFn: ({ bookmarkId, description }) =>
       updateBookmark(contentId, bookmarkId, description),
-    onSuccess: () => {
+    // when mutation is called:
+    onMutate: async ({ bookmarkId, description }) => {
+      await queryClient.cancelQueries({ queryKey: ['bookmarks', contentId] });
+      const previousBookmarks =
+        queryClient.getQueryData<BookmarkByContentIdResponse>([
+          'bookmarks',
+          contentId,
+        ]);
+      // 캐시에 optimistically update
+      queryClient.setQueryData(['bookmarks', contentId], {
+        ...previousBookmarks,
+        data: {
+          bookmarkList: [
+            ...(previousBookmarks?.data.bookmarkList.map((bookmark) => {
+              // 해당 bookmarkId에 해당하는 북마크의 description을 업데이트
+              if (bookmark.bookmarkId === bookmarkId) {
+                return {
+                  ...bookmark, // 기존의 모든 속성 유지
+                  description, // 새로운 description으로 업데이트
+                };
+              }
+              return bookmark; // 변동 없는 북마크는 그대로 반환
+            }) || []),
+          ],
+        },
+      });
+    },
+    // 에러가 발생할 경우 onMute에서 보관한 데이터가 있다면 복원
+    onError: (err, bookmarkId, context) => {
+      const ctx = context as { previousBookmarks?: BookmarkByContentId[] };
+
+      if (ctx.previousBookmarks) {
+        queryClient.setQueryData(
+          ['bookmarks', contentId],
+          ctx.previousBookmarks,
+        );
+      }
+    },
+    // 성공 여부와 관계없이 항상 refetch
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ['bookmarks', contentId] });
     },
   });
 };
 
-// 북마크 삭제 훅
+// 북마크 삭제 훅 : optimistic update적용
 export const useDeleteBookmark = (contentId: number) => {
   const queryClient = useQueryClient();
 
   return useMutation<void, Error, number>({
     mutationFn: (bookmarkId) => deleteBookmark(bookmarkId),
-    // TODO(@godhyzzang) : onError처리
+    // when mutation is called:
+    onMutate: async (bookmarkId) => {
+      await queryClient.cancelQueries({ queryKey: ['bookmarks', contentId] });
+      const previousBookmarks =
+        queryClient.getQueryData<BookmarkByContentIdResponse>([
+          'bookmarks',
+          contentId,
+        ]);
+      // 캐시에 optimistically update
+      queryClient.setQueryData(['bookmarks', contentId], {
+        ...previousBookmarks,
+        data: {
+          bookmarkList: [
+            ...(previousBookmarks?.data.bookmarkList.filter(
+              (bookmark) => bookmark.bookmarkId !== bookmarkId,
+            ) || []),
+          ],
+        },
+      });
+      return { previousBookmarks };
+    },
+    // 에러가 발생할 경우 onMute에서 보관한 캐시데이터가 있다면 복원
+    onError: (err, bookmarkId, context) => {
+      const ctx = context as { previousBookmarks?: BookmarkByContentId[] };
+
+      if (ctx.previousBookmarks) {
+        queryClient.setQueryData(
+          ['bookmarks', contentId],
+          ctx.previousBookmarks,
+        );
+      }
+    },
+    // 성공 여부와 관계없이 항상 refetch
     onSettled: async () => {
       const result = await queryClient.invalidateQueries({
         queryKey: ['bookmarks', contentId],
