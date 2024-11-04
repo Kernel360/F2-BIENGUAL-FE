2f4a22633b644c4d8f3e6ed0821e2bf851f6ac6c
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-11-04 18:36:17 +0900

    remove: 필요없는 div태그 삭제

--- src/app/(default)/dashboard/page.tsx
+++ src/app/(default)/dashboard/page.tsx
@@ -5,7 +5,6 @@ import Calendar from '@/components/common/DashboardCalendar';
 export default function Dashboardpage() {
   return (
     <div className="border p-3">
-      <span className="">월간학습</span>
       <Calendar />
     </div>
   );
