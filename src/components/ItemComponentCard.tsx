1cf08140d95c4ff1baf4cf2f452ee4c2706b417c
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-11-14 13:27:38 +0900

    design : 컨텐트타입 뱃지 추가

--- src/components/ItemComponentCard.tsx
+++ src/components/ItemComponentCard.tsx
@@ -2,7 +2,7 @@
 
 'use client';
 
-import { Clock } from 'lucide-react';
+import { BookOpen, Clock, Headphones } from 'lucide-react';
 
 import PointCover from './common/PointCover';
 import ContentCard from './items/ContentCard';
@@ -26,13 +26,28 @@ export default function ItemComponentCard({ data }: { data: any }) {
           />
         }
         bottomRightButton={
-          <Badge className="flex items-center gap-1 bg-gray-200 bg-opacity-70">
-            <Clock className="w-3 h-3" color="purple" />
-            <span className="text-violet-800">24:00</span>
-          </Badge>
+          data.contentType !== 'READING' && (
+            <Badge className="flex items-center gap-1 bg-gray-200 bg-opacity-70">
+              <Clock className="w-3 h-3" color="purple" />
+              <span className="text-violet-800">{data.duration}</span>
+            </Badge>
+          )
         }
         coverImageUrl={data.thumbnailUrl}
-        leftBadge={<Badge>{data.category}</Badge>}
+        leftBadge={
+          <div className="flex gap-1">
+            <Badge className="">{data.category}</Badge>
+            {data.contentType !== 'READING' ? (
+              <div className="flex justify-center items-center bg-gradient-to-l from-red-500 to-orange-500 rounded-sm shadow-sm">
+                <Headphones className="p-1 h-6 w-6 text-white" />
+              </div>
+            ) : (
+              <div className="flex justify-center items-center bg-gradient-to-l from-blue-500 to-sky-500 rounded-sm shadow-sm  ">
+                <BookOpen className="p-1 h-6 w-6  text-white" />
+              </div>
+            )}
+          </div>
+        }
         rightBadge={`조회수 ${data.hits}`}
         title={data.title}
         description={data.preScripts}
