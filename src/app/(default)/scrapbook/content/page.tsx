ed92f227d8ea6c1048b4f3fa5940a7b18380ca5b
smosco <go1ruf2tk3@gmail.com>
2024-11-19 10:14:55 +0900

    refactor: 날짜 포매팅 함수 적용

--- src/app/(default)/scrapbook/content/page.tsx
+++ src/app/(default)/scrapbook/content/page.tsx
@@ -7,7 +7,7 @@ import EmptyAlert from '@/components/common/EmptyAlert';
 import LoadingSpinner from '@/components/common/LoadingSpinner';
 import ContentCard from '@/components/items/ContentCard';
 import { Badge } from '@/components/ui/badge';
-import { formatFullDateWithPad } from '@/lib/formatDate';
+import { formatDate } from '@/lib/formatDate';
 
 export default function RecentContent() {
   const { data: allScrapData, isLoading, isError, error } = useFetchScrap();
@@ -53,7 +53,7 @@ export default function RecentContent() {
           }
           rightBadge={
             <span className="text-sm text-muted-foreground">
-              {formatFullDateWithPad(item.createdAt)} 저장
+              {formatDate(item.createdAt, 'YYYY.MM.DD')} 저장
             </span>
           }
           bottomRightButton={
