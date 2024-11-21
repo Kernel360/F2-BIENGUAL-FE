40775e9d0e1a6b4b1a2dc729395dba7272468e69
smosco <go1ruf2tk3@gmail.com>
2024-11-21 17:50:47 +0900

    feat: 다른 사람들이 저장한 문장 컴포넌트 width 수정

--- src/components/SentenceComponent.tsx
+++ src/components/SentenceComponent.tsx
@@ -55,27 +55,27 @@ export default function SentenceComponent({ data }: { data: Sentence }) {
   const [showTranslation, setShowTranslation] = useState(false);
 
   return (
-    <Card className="w-[783px] h-full bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 shadow-xl rounded-lg">
+    <Card className="w-[794px] h-full bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 shadow-xl rounded-lg">
       <CardContent className="p-8 flex flex-col justify-between h-full">
         <div className="flex justify-between items-start">
           <div>
             <h3 className="text-2xl font-bold text-white mb-4">
               다른 사람들이 저장한 문장
             </h3>
-            <p className="text-xl text-white mb-6">{data.english}</p>
+            <p className="text-lg text-white mb-4">{data.english}</p>
             {showTranslation && (
               <p className="text-lg text-gray-200">{data.korean}</p>
             )}
           </div>
-          <div className="text-right">
-            {/* 문장이 있는 해당 페이지로 가는 <a/> tag */}
+          {/* 문장이 있는 해당 페이지로 가는 <a/> tag */}
+          {/* <div className="text-right">
             <a
               href={`/content/${data.english}`}
               className="text-sm text-blue-300 underline"
             >
               Go to Page
             </a>
-          </div>
+          </div> */}
         </div>
         <div className="flex justify-between items-center mt-6">
           <Button
