92eb834d2ed8c4b9c8db8526a267d307499c5293
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-11-25 18:21:28 +0900

    [fix]  나중에 고를래요 누르면 빈 배열 보내게 만들기 (#312)
    * fix : 나중에 고르기 누르면 빈 배열 보내서 나중에 로그인했을 때 login/add페이지 다시 안 뜨게 만들기, 버튼 제출하고 이동하는 동안 로딩스피너 뜨게만들기
    
    * design : loading스피너가 백지에 보이는 문제 해결
    
    * fix : 이 페이지는 페이지 라우팅 스택에 안 쌓이도록 만들기
    
    * chore : state 이름 수정, onError일 때 toast알림 띄우도록 변경

--- src/app/(fullscreen)/login/add/page.tsx
+++ src/app/(fullscreen)/login/add/page.tsx
@@ -16,11 +16,11 @@ export default function LoginAddPage() {
   const { data, isLoading } = useFetchAllCategories();
 
   const [categories, setCategories] = useState<CategoryList[]>([]);
-
   const [selectedCategories, setSelectedCategories] = useState<number[]>([]);
+  const [isSubmitting, setIsSubmitting] = useState(false); // 버튼 누르고 성공 응답 오기 전에 로딩 스피너 띄우기
+
   const updateUserInfoMutation = useUpdateUserInfo();
   const router = useRouter();
-
   const toast = useToast();
 
   useEffect(() => {
@@ -34,18 +34,41 @@ export default function LoginAddPage() {
       updateUserInfoMutation.mutate(
         { categories: selectedCategories },
         {
+          onSettled: () => {
+            setIsSubmitting(true); // 버튼 누르고 성공 응답 오기 전에 로딩 스피너 띄우기
+          },
           onSuccess: () => {
-            router.push('/');
+            router.replace('/');
           },
           onError: (error) => {
             console.log(error);
+            toast.toast({
+              description: `에러가 발생하였습니다 : ${error.message}`,
+            });
           },
         },
       );
     }
   };
-  const notChoosingCategories = async () => {
-    router.push('/');
+
+  const notChoosingCategories = () => {
+    updateUserInfoMutation.mutate(
+      { categories: [] },
+      {
+        onSettled: () => {
+          setIsSubmitting(true); // 버튼 누르고 성공 응답 오기 전에 로딩 스피너 띄우기
+        },
+        onSuccess: () => {
+          router.replace('/');
+        },
+        onError: (error) => {
+          console.log(error);
+          toast.toast({
+            description: `에러가 발생하였습니다 : ${error.message}`,
+          });
+        },
+      },
+    );
   };
 
   const toggleCategory = (categoryId: number) => {
@@ -65,12 +88,8 @@ export default function LoginAddPage() {
     });
   };
 
-  return isLoading ? (
-    <div className="flex justify-center items-center h-screen w-full">
-      <LoadingSpinner />
-    </div>
-  ) : (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-indigo-50 to-violet-50">
+  return (
+    <div className="relative flex items-center justify-center min-h-screen bg-gradient-to-br from-indigo-50 to-violet-50">
       <Card className="h-84 mx-8 my-8 py-6 bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
         <p className="text-2xl font-bold py-6 mb-6 text-center text-violet-600  dark:text-gray-100">
           <br />
@@ -84,7 +103,7 @@ export default function LoginAddPage() {
           </span>
         </p>
 
-        <div className="grid md:grid-cols-3 sm:grid-cols-2  gap-4 mx-44">
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-4 mx-44">
           {categories.map((category) => (
             <Button
               type="button"
@@ -101,7 +120,7 @@ export default function LoginAddPage() {
           ))}
         </div>
 
-        <div className=" flex mt-20 mx-96 gap-2 ">
+        <div className="flex mt-20 mx-96 gap-2">
           <Button
             onClick={notChoosingCategories}
             className="bg-violet-100 text-gray px-6 py-2 rounded-3xl text-lg font-semibold transition-all duration-200 ease-in-out hover:scale-105 hover:bg-violet-200/90"
@@ -110,12 +129,18 @@ export default function LoginAddPage() {
           </Button>
           <Button
             onClick={handlePreferedCategories}
-            className="bg-violet-600 px-6 py-2  rounded-3xl text-lg font-semibold transition-all duration-200 ease-in-out hover:scale-105 hover:shadow-lg"
+            className="bg-violet-600 px-6 py-2 rounded-3xl text-lg font-semibold transition-all duration-200 ease-in-out hover:scale-105 hover:shadow-lg"
           >
             다 골랐어요
           </Button>
         </div>
       </Card>
+
+      {(isLoading || isSubmitting) && (
+        <div className="absolute inset-0 flex justify-center items-center bg-white bg-opacity-50">
+          <LoadingSpinner />
+        </div>
+      )}
     </div>
   );
 }
