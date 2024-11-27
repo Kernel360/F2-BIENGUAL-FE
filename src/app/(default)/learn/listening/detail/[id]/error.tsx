766fad30f69b1bf9173e5d417f83087e886c0add
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-11-27 16:42:47 +0900

    chore : 쓸데없는 주석 삭제

--- src/app/(default)/learn/listening/detail/[id]/error.tsx
+++ src/app/(default)/learn/listening/detail/[id]/error.tsx
@@ -12,7 +12,6 @@ export default function Error({
   const router = useRouter();
 
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error);
     router.push('/');
   }, [error]);
