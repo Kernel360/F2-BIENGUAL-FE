62ae6f61fa0f667545d75c402194bcc1dd53edca
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-11-27 16:42:01 +0900

    fix : 홈으로 되돌아가게 바꾸기

--- src/app/(default)/learn/reading/detail/[id]/error.tsx
+++ src/app/(default)/learn/reading/detail/[id]/error.tsx
@@ -14,6 +14,6 @@ export default function Error({
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error);
-    router.back();
+    router.push('/');
   }, [error]);
 }
