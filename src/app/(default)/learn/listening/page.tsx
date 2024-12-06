c73f23d4c0d6a825305906601ae3c7497cb916eb
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-12-06 11:05:20 +0900

    comment : 주석 수정

--- src/app/(default)/learn/listening/page.tsx
+++ src/app/(default)/learn/listening/page.tsx
@@ -16,28 +16,26 @@ export default async function ListeningPage({
 }: {
   searchParams: Record<string, string | undefined>;
 }) {
-  console.log('searchParams', searchParams);
+  // console.log('searchParams', searchParams);
 
-     // 서버 컴포넌트라 useQueryClient 사용불가하므로 QueryClient 새로 생성
+  // 서버 컴포넌트라 useQueryClient 사용불가하므로 QueryClient 새로 생성
+  const queryClient = new QueryClient();
 
-     const queryClient = new QueryClient();
-     // 카테고리 데이터 미리 가져오기
-     await queryClient.prefetchQuery({
+  await queryClient.prefetchQuery({
        queryKey: ['categories', 'LISTENING'],
        queryFn: () => fetchCategoriesByContentType('LISTENING'),
      });
 
   // 안전한 기본값 처리
-   const page = Number(searchParams?.page || '1'); // 기본값 1
-   const size = Number(searchParams?.size || '10'); // 기본값 10
-   const sort = searchParams?.sort || 'createdAt'; // 기본값 createdAt
-   const direction = searchParams?.direction || 'DESC'; // 기본값 DESC
-   const categoryId = searchParams?.categoryId || ''; // categoryId는 undefined 허용
-   console.log('categoryId', categoryId);
-   const categoryIdNumber = categoryId ? Number(categoryId) : undefined;
-   console.log('categoryIdNumber', categoryIdNumber);
+   const page = Number(searchParams?.page || '1');
+   const size = Number(searchParams?.size || '10');
+   const sort = searchParams?.sort || 'createdAt';
+   const direction = searchParams?.direction || 'DESC';
+   const categoryId = searchParams?.categoryId || '';
+  //  console.log('categoryId', categoryId);
 
- 
+   const categoryIdNumber = categoryId ? Number(categoryId) : undefined;
+  //  console.log('categoryIdNumber', categoryIdNumber);
 
   await queryClient.prefetchQuery({
     queryKey: [
--- src/app/(default)/learn/reading/page.tsx
+++ src/app/(default)/learn/reading/page.tsx
@@ -15,9 +15,7 @@ export default async function ReadingPage({
 }: {
   searchParams: Record<string, string | undefined>;
 }) {
-  console.log('searchParams', searchParams);
-
-
+  // console.log('searchParams', searchParams);
 
    // 서버 컴포넌트라 useQueryClient 사용불가하므로 QueryClient 새로 생성
    const queryClient = new QueryClient();
@@ -29,14 +27,15 @@ export default async function ReadingPage({
  
 
   // 안전한 기본값 처리
-   const page = Number(searchParams?.page || '1'); // 기본값 1
-   const size = Number(searchParams?.size || '10'); // 기본값 10
-   const sort = searchParams?.sort || 'createdAt'; // 기본값 createdAt
-   const direction = searchParams?.direction || 'DESC'; // 기본값 DESC
-   const categoryId = searchParams?.categoryId || ''; // categoryId는 undefined 허용
-   console.log('categoryId', categoryId);
+   const page = Number(searchParams?.page || '1'); 
+   const size = Number(searchParams?.size || '10'); 
+   const sort = searchParams?.sort || 'createdAt'; 
+   const direction = searchParams?.direction || 'DESC'; 
+   const categoryId = searchParams?.categoryId || '';
+  //  console.log('categoryId', categoryId);
+
    const categoryIdNumber = categoryId ? Number(categoryId) : undefined;
-   console.log('categoryIdNumber', categoryIdNumber);
+  //  console.log('categoryIdNumber', categoryIdNumber);
 
 
   await queryClient.prefetchQuery({
