5e28909da5f8b609ac88127df58528d1918d8d38
smosco <go1ruf2tk3@gmail.com>
2024-12-06 17:20:26 +0900

    feat: 오프라인이고 캐시가 없을 때 보여줄 html 파일 생성 및 캐시

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,36 @@
+// install 이벤트: offline.html 캐싱
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('cache-v1').then((cache) => {
+      return cache.addAll([
+        '/offline.html', // offline.html 파일을 캐싱
+      ]);
+    }),
+  );
+  console.log('Service Worker 설치 완료');
+});
+
+// fetch 이벤트
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request) // 네트워크에서 먼저 시도
+    fetch(event.request)
       .then(async (response) => {
-        // console.log(response);
-        // 네트워크 응답이 성공하면 캐시에 저장
+        // 네트워크 요청 성공 시 캐시에 저장
         const cache = await caches.open('cache-v1');
-        cache.put(event.request, response.clone()); // 응답 캐시에 저장
+        cache.put(event.request, response.clone());
         return response;
       })
       .catch(async () => {
-        // 네트워크 요청 실패 시 캐시에서 데이터 반환
+        // 네트워크 요청 실패 시 캐시에서 offline.html 반환
         const cache = await caches.open('cache-v1');
         const cachedResponse = await cache.match(event.request);
         if (cachedResponse) {
           console.log('캐시에서 응답 반환');
           return cachedResponse;
         }
-        // 캐시에도 없으면 기본 응답 반환
+        // offline.html을 캐시에서 반환
         console.log('네트워크와 캐시 모두 실패');
-        return new Response('오프라인 상태이며 캐시 데이터 없음', {
-          status: 503,
-          statusText: 'Service Unavailable',
-        });
+        return cache.match('/offline.html');
       }),
   );
 });
