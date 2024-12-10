eda4da2dcbe7ccb3120c03d5366310e073ec9bd6
smosco <go1ruf2tk3@gmail.com>
2024-12-10 16:55:18 +0900

    refactor: 카테고리 추천 데이터 초기값 서버에서 패치해옴

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -4,25 +4,37 @@ import {
   fetchReadingPreview,
   fetchListeningPreview,
 } from '@/api/queries/contentsQueries';
-import { fetchRecommendedBookmarks } from '@/api/queries/recommendQueries';
+import {
+  fetchRecommendedBookmarks,
+  fetchRecommendedContents,
+} from '@/api/queries/recommendQueries';
+import { mockRecommendedData } from '@/lib/constants/mockRecommendedData';
 
 import HomePageClient from '../../components/HomePageClient';
 
 export default async function HomePage() {
-  const initialReadingContents = await fetchReadingPreview({
-    Cookie: cookies().toString(),
-  });
-  const initialListeningContents = await fetchListeningPreview({
-    Cookie: cookies().toString(),
-  });
-  // TODO(@godhyzzang) : 추천bookmark한 문장도  initialData연결
-  const initialSentences = await fetchRecommendedBookmarks();
+  const cookieHeader = cookies().toString();
+
+  const [
+    initialReadingContents,
+    initialListeningContents,
+    initialSentences,
+    initialRecommendedContents,
+  ] = await Promise.all([
+    fetchReadingPreview({ Cookie: cookieHeader }),
+    fetchListeningPreview({ Cookie: cookieHeader }),
+    fetchRecommendedBookmarks(),
+    cookieHeader
+      ? fetchRecommendedContents({ Cookie: cookieHeader }) || []
+      : mockRecommendedData,
+  ]);
 
   return (
     <HomePageClient
       initialReadingContents={initialReadingContents}
       initialListeningContents={initialListeningContents}
       initialSentences={initialSentences}
+      initialRecommendedContents={initialRecommendedContents}
     />
   );
 }
