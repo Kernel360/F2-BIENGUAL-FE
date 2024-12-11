a7616c7a6e59a4758983bb1e4e5e471131a1aa47
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-12-11 15:39:26 +0900

    design: level 색상 변화

--- src/components/DifficultyDisplay.tsx
+++ src/components/DifficultyDisplay.tsx
@@ -1,5 +1,4 @@
 import { QuestionMarkCircledIcon } from '@radix-ui/react-icons';
-import { Heart } from 'lucide-react';
 
 import { LevelType } from '@/types/Level';
 
@@ -9,36 +8,33 @@ export default function DifficultyDisplay({
   calculatedLevel: LevelType;
 }) {
   const getHeartCount = (level: LevelType) => {
-    if (level === 'LOW') return 1;
-    if (level === 'MEDIUM') return 2;
-    return 3;
+    if (level === 'LOW') return 'EASY';
+    if (level === 'MEDIUM') return 'MEDIUM';
+    return 'HARD';
+  };
+
+  const getBackgroundColor = (level: LevelType) => {
+    if (level === 'LOW') return 'bg-green-500/70';
+    if (level === 'MEDIUM') return 'bg-yellow-500/70';
+    return 'bg-red-500/70';
   };
 
   return (
     <div className="flex h-full w-full items-center justify-start">
-      {calculatedLevel ? (
-        <div className="flex items-center justify-start w-full ">
-          <span className="flex items-center justify-center text-center p-1 rounded">
-            {Array.from({ length: getHeartCount(calculatedLevel) }).map(
-              (_, index: number) => (
-                // eslint-disable-next-line react/no-array-index-key
-                <Heart key={index} fill="#7641ed" stroke="none" />
-              ),
-            )}
-          </span>
-        </div>
-      ) : (
-        <div className="relative flex items-center justify-center w-auto h-full">
-          <div className="flex items-center justify-center p-1 px-2 gap-1 text-sm text-white bg-violet-500/30 rounded">
+      <div className="relative flex items-center justify-center w-auto h-full">
+        {!calculatedLevel ? (
+          <div
+            className={`flex items-center justify-center p-1 px-2 mx-1 gap-1 text-xs font-semibold font-mono text-white ${getBackgroundColor(calculatedLevel)} backdrop-blur-sm rounded`}
+          >
+            <span>{getHeartCount(calculatedLevel)}</span>
+          </div>
+        ) : (
+          <div className="flex items-center justify-center  p-1 px-2 mx-1 gap-1 text-xs font-semibold font-mono text-white bg-violet-500/70  backdrop-blur-sm rounded">
             LEVEL
             <QuestionMarkCircledIcon />
           </div>
-          {/* <QuestionMarkCircledIcon
-            className="h-4 w-4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
-            stroke="#7641ED"
-          /> */}
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
