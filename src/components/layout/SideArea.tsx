1b6088812e6aaa22a4172d3deefd75ddcd63b381
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-12-13 17:53:45 +0900

    css : 사이드에어리어 너무 큰 마진 조정

--- src/components/layout/SideArea.tsx
+++ src/components/layout/SideArea.tsx
@@ -6,7 +6,7 @@ import LearningTracker from './side/LearningTracker';
 
 export default function SideArea() {
   return (
-    <div className="hidden md:block h-fit w-[260px] ml-[50px] py-[60px]">
+    <div className="hidden md:block h-fit w-[260px] m-5 py-[60px]">
       <LearningTracker />
     </div>
   );
