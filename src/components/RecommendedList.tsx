62177a397ee374a538cd208a25fca8e366738e88
smosco <go1ruf2tk3@gmail.com>
2024-12-15 18:07:14 +0900

    fix: 추천 콘텐츠 gap 수정

--- src/components/RecommendedList.tsx
+++ src/components/RecommendedList.tsx
@@ -35,7 +35,7 @@ export default function RecommendedList({
         )}
 
         {/* 추천 콘텐츠 리스트 */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {recommendedData.map((content, index) => (
             <Link
               key={content.contentId}
