5bc6bf42ba842f86dacef001ff56ce7b2895096f
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-12-16 18:03:08 +0900

    fix : floating button 20px씩 올림

--- src/app/(default)/learn/listening/detail/ListeningDetailClient.tsx
+++ src/app/(default)/learn/listening/detail/ListeningDetailClient.tsx
@@ -346,7 +346,7 @@ export default function ListeningDetailClient({
         onScrapToggle={handleScrapToggle}
       />
 
-      <div className="fixed right-4 bottom-20 md:bottom-4">
+      <div className="fixed right-4 bottom-25 md:bottom-10">
         <Button
           variant="default"
           size="icon"
