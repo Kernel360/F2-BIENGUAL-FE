04373c5ba3d7d3dc122520acb549f30d6d3b2273
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-12-16 15:16:17 +0900

    Revert "fix : logout일 때 추천리스트에 mockdata안 보여주는 문제 해결"
    This reverts commit e911dc07b7e8134af2f1ea48b783f386a6d72532.

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -57,9 +57,7 @@ export default async function HomePage() {
       : {
           code: 'error',
           message: '추천 콘텐츠 조회 실패',
-          data: {
-            recommendedContents: mockRecommendedData.data.recommendedContents,
-          },
+          data: { recommendedContents: [] },
         };
 
   return (
